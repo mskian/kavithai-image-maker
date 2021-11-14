41a6eb54f42f7046497eaf1e698120d324d50554
mskian <veerfun23@gmail.com>
2021-11-14 15:10:16 +0530

    update readme and csrf error message

--- index.js
+++ index.js
@@ -69,3 +69,10 @@ app.use('/', function(req, res) {
         message: 'Web App Error'
     });
 });
+app.use(function(err, req, res, next) {
+    if (err.code !== 'EBADCSRFTOKEN') return next(err)
+    res.status(403).json({
+        error: 1,
+        message: 'Token Error'
+    });
+})
