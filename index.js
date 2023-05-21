22b6fab82d4af5ab3d5b6bea293ff42500c4bc21
mskian <10300271+mskian@users.noreply.github.com>
2023-05-21 10:16:57 +0530

    Update font size and add more colors

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function random_item(items) {
     return items[Math.floor(Math.random() * items.length)];
 
 }
-const items = ['#E9D5FF', '#FEF08A', '#BBF7D0', '#DFE6E9'];
+const items = ['#E9D5FF', '#FEF08A', '#BBF7D0', '#DFE6E9', '#f1f2f6', '#78e08f', '#b8e994', '#ffcccc', '#cd84f1'];
 
 app.get('/', csrfProtection, function(req, res) {
     res.header('X-Frame-Options', 'DENY');
@@ -109,7 +109,7 @@ app.post('/kavithai', csrfProtection, [
             },
             type: 'jpeg',
             quality: 70,
-            html: `<html> <head> <link rel="preconnect" href="https://fonts.googleapis.com"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700&display=swap" rel="stylesheet"> <style>body{width: 1080px; height: 1080px; background-color: ${random_item(items)}; font-family: 'Baloo Thambi 2', cursive; font-weight: 700;}p{color: #222222; font-size: 38px; font-family: 'Baloo Thambi 2', cursive; font-weight: 700; line-height: 1.6; text-align: center; white-space: pre-wrap;}.container{height: 96%; width: 96%; display: flex; position: fixed; align-items: center; justify-content: center;}#footer{position:absolute; bottom:0; width:100%; height: 60px; text-align: center; align-items: center; justify-content: center; color: #222222; font-size: 32px; font-family: 'Baloo Thambi 2', cursive; font-weight: 700; line-height: 1.6;}</style> </head> <body> <div class="container"> <p class="card">{{content}}</p></div><div id="footer">{{author}}</div><br></body> </html>`
+            html: `<html> <head> <link rel="preconnect" href="https://fonts.googleapis.com"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700&display=swap" rel="stylesheet"> <style>body{width: 1080px; height: 1080px; background-color: ${random_item(items)}; font-family: 'Baloo Thambi 2', cursive; font-weight: 700;}p{color: #222222; font-size: 38px; font-family: 'Baloo Thambi 2', cursive; font-weight: 700; line-height: 1.6; text-align: center; white-space: pre-wrap;}.container{height: 96%; width: 96%; display: flex; position: fixed; align-items: center; justify-content: center;}#footer{position:absolute; bottom:0; width:100%; height: 60px; text-align: center; align-items: center; justify-content: center; color: #222222; font-size: 35px; font-family: 'Baloo Thambi 2', cursive; font-weight: 700; line-height: 1.6;}</style> </head> <body> <div class="container"> <p class="card">{{content}}</p></div><div id="footer">{{author}}</div><br></body> </html>`
         })
         const b64 = Buffer.from(image).toString('base64');
         res.render('download', {
